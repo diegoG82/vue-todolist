39e8a187e51fc0d2f164f4499b0a59d4706487da
diegoG82 <diegogiardina@hotmail.it>
2023-03-30 18:14:21 +0200

    aggiunto unshift al push delle tasktodo

--- js/script.js
+++ js/script.js
@@ -34,7 +34,7 @@ createApp({
     // Predisporre un campo di input testuale e un pulsante "aggiungi": cliccando sul pulsante, il testo digitato viene letto e utilizzato per creare un nuovo todo, che quindi viene aggiunto alla lista dei todo esistenti.
     addTodo() {
       if (this.newTodos.text !== "") {
-        this.tasksToDo.push({ ...this.newTodos });
+        this.tasksToDo.unshift({ ...this.newTodos });
       }
     },
   },
